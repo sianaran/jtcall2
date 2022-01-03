6ead757aae76ef0c6bb95bb7cb1761a92b9365c3
sianaran <sianaran@gmail.com>
2022-01-03 17:31:38 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,22 @@
-<?php echo "ยินดีต้อนรับ";?>
+<?php
+$input=file_get_contents('php://input');
+$data=json_decode($input);
+
+$chatId = $data->message->chat->id;
+$text = $data->message->text;
+
+$telegramtoken = "1474434301:AAEuCjbRgID56XcvzdeWuxlGW-34Zl5-RT8"; //
+//$telegramchatid = "-380293919"; //ซีรีย์ตอนใหม่
+function sendMessagetelegram($chatId, $text, $telegramtoken) {
+    $url = "https://api.telegram.org/bot" . $token . "/sendMessage?chat_id=" . $chatID;
+    $url = $url . "&text=" . urlencode($messaggio);
+    $ch = curl_init();
+    $optArray = array(
+            CURLOPT_URL => $url,
+            CURLOPT_RETURNTRANSFER => true
+    );
+    curl_setopt_array($ch, $optArray);
+    $result = curl_exec($ch);
+    curl_close($ch);
+}
+?>
